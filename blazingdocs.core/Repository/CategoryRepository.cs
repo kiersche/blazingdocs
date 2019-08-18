29cf2616bc6b86407e5db290632211f811efee1d
Stefan Kirschner <s-kirschner@web.de>
2019-08-18 11:55:19 +0200

    CategoryRepository hinzugefügt

---
+++ blazingdocs.core/Repository/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using blazingdocs.core.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace blazingdocs.core.Repository
+{
+    public interface ICategoryRepository
+    {
+        Task AddCategory(Category category);
+        Task<List<Category>> GetAllCategories();
+        Task<Category> GetCategoryById(int id);
+    }
+
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly DmsDbContext dmsDbContext;
+
+        public CategoryRepository(DmsDbContext dmsDbContext)
+        {
+            this.dmsDbContext = dmsDbContext ?? throw new ArgumentNullException(nameof(dmsDbContext));
+        }
+
+        public async Task AddCategory(Category category)
+        {
+            dmsDbContext.Add<Category>(category);
+            await dmsDbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Category>> GetAllCategories()
+        {
+            return await dmsDbContext.Categories.AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryById(int id)
+        {
+            return await dmsDbContext.Categories.AsNoTracking()
+                .Where(_ => _.CategoryId == id)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
