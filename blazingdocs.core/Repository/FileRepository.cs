fea405c327b0b1a565150dfc07fc32f8254fb47a
Stefan Kirschner <s-kirschner@web.de>
2019-09-09 20:22:02 +0200

    Repositories hinzugefügt. Fühlt sich irgendwie komisch an, als müsste ich es eigentlich anders angehen. Einfach auf Verdacht bestimmte Grundoperationen für jede Entity anzulegen... vllt wäre ein Top-Down approach besser gewesen

---
+++ blazingdocs.core/Repository/FileRepository.cs
@@ -0,0 +1,45 @@
+﻿using blazingdocs.core.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blazingdocs.core.Repository
+{
+    public interface IFileRepository
+    {
+        Task AddFile(File file);
+        Task UpdateFileValues(int id, Action<File> valueUpdater);
+    }
+
+    public class FileRepository : IFileRepository
+    {
+        private readonly DmsDbContext dmsDbContext;
+        private readonly IEntityPropertyUpdater<File, int> entityPropertyUpdater;
+
+        public FileRepository(DmsDbContext dmsDbContext,
+            IEntityPropertyUpdater<File, int> entityPropertyUpdater)
+        {
+            this.dmsDbContext = dmsDbContext ?? throw new ArgumentNullException(nameof(dmsDbContext));
+            this.entityPropertyUpdater = entityPropertyUpdater ?? throw new ArgumentNullException(nameof(entityPropertyUpdater));
+            this.entityPropertyUpdater.EntityFactoryFunc = id => new File { FileId = id };
+        }
+
+        public async Task AddFile(File file)
+        {
+            dmsDbContext.Add<File>(file);
+            await dmsDbContext.SaveChangesAsync();
+        }
+
+        public async Task<File> GetFileById(int id)
+        {
+            return await dmsDbContext.Files.FirstOrDefaultAsync(_ => _.FileId == id);
+        }
+
+        public async Task UpdateFileValues(int id, Action<File> valueUpdater)
+        {
+            await entityPropertyUpdater.UpdateEntity(id, valueUpdater);
+        }
+    }
+}
---
+++ blazingdocs.core/Repository/PhysicalDocumentContainerRepository.cs
@@ -0,0 +1,49 @@
+﻿using blazingdocs.core.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blazingdocs.core.Repository
+{
+    public interface IPhysicalDocumentContainerRepository
+    {
+        Task AddContainer(PhysicalDocumentContainer container);
+        Task<PhysicalDocumentContainer> GetById(int id);
+        Task UpdateContainerValues(int id, Action<PhysicalDocumentContainer> valueUpdater);
+    }
+
+    public class PhysicalDocumentContainerRepository : IPhysicalDocumentContainerRepository
+    {
+        private readonly DmsDbContext dbContext;
+        private readonly IEntityPropertyUpdater<PhysicalDocumentContainer, int> entityPropertyUpdater;
+
+        public PhysicalDocumentContainerRepository(DmsDbContext dbContext,
+            IEntityPropertyUpdater<PhysicalDocumentContainer, int> entityPropertyUpdater)
+        {
+            this.dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            this.entityPropertyUpdater = entityPropertyUpdater ?? throw new ArgumentNullException(nameof(entityPropertyUpdater));
+            this.entityPropertyUpdater.EntityFactoryFunc = id => new PhysicalDocumentContainer { PhysicalDocumentContainerId = id };
+        }
+
+        public async Task AddContainer(PhysicalDocumentContainer container)
+        {
+            dbContext.Add(container);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<PhysicalDocumentContainer> GetById(int id)
+        {
+            return await dbContext.PhysicalDocumentContainers.AsNoTracking()
+                .Where(_ => _.PhysicalDocumentContainerId == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateContainerValues(int id, Action<PhysicalDocumentContainer> valueUpdater)
+        {
+            await entityPropertyUpdater.UpdateEntity(id, valueUpdater);
+        }
+    }
+}
