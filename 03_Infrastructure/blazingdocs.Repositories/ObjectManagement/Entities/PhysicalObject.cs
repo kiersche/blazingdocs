f1bccc21ee784cc25c707898e1a7a6c7ff30e241
Stefan Kirschner <s-kirschner@web.de>
2021-05-24 19:41:14 +0200

    Cleanup

--- 03_Infrastructure/blazingdocs.Repositories/ObjectManagement/Entities/PhysicalObject.cs
+++ 03_Infrastructure/blazingdocs.Repositories/ObjectManagement/Entities/PhysicalObject.cs
@@ -7,10 +7,10 @@ namespace blazingdocs.core.Model
     public class PhysicalObject
     {
         public int PhysicalObjectId { get; set; }
+        public int IndexInContainer { get; set; }
 
         public int PhysicalObjectContainerId { get; set; }
         public PhysicalObjectContainer PhysicalObjectContainer { get; set; }
-        public int IndexInContainer { get; set; }
 
         public int ObjectId { get; set; }
         public Object Object { get; set; }
