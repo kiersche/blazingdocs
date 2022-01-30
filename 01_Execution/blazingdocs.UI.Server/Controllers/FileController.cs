e33e91276c4d3f73b4c9f7e428058dbc4337431e
Stefan Kirschner <s-kirschner@web.de>
2022-01-30 19:22:41 +0100

    Refactoring

--- 01_Execution/blazingdocs.UI.Server/Controllers/FileController.cs
+++ 01_Execution/blazingdocs.UI.Server/Controllers/FileController.cs
@@ -1,26 +1,32 @@
 ﻿using blazingdocs.Contracts;
+using blazingdocs.Contracts.Files;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace blazingdocs.UI.Server.Controllers
+namespace blazingdocs.UI.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FileController : Controller
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class FileController : Controller
+    private readonly IOptionsMonitor<ConfigurationContract> options;
+
+    public FileController(IOptionsMonitor<ConfigurationContract> options)
     {
-        private readonly IOptionsMonitor<ConfigurationContract> options;
+        this.options = options;
+    }
 
-        public FileController(IOptionsMonitor<ConfigurationContract> options)
-        {
-            this.options = options;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    [HttpPost]
+    public async Task<ActionResult<List<FileUploadResultContract>>> PostFile([FromForm] IEnumerable<IFormFile> files)
+    {
 
-        [HttpGet("list-unassigned")]
-        public 
     }
 }
